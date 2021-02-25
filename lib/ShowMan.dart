ac9712b6934ef19181327e1c400bb9b6e3d3e517
donizetemelocoutinho <donizete.coutinho@agilefacil.com.br>
2021-02-25 14:03:06 -0300

    Teste de Plugion...

---
+++ lib/ShowMan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowMan extends StatefulWidget {
+  @override
+  _ShowManState createState() => _ShowManState();
+}
+
+class _ShowManState extends State<ShowMan> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Show Man...."),
+    );
+  }
+}
