551bf96ad82dee328b4b3c91640edbd90c69ab3b
donizetemelocoutinho <donizete.coutinho@agilefacil.com.br>
2021-02-25 14:15:06 -0300

    Teste de Plugion...

--- lib/ShowMan.dart
+++ lib/ShowMan.dart
@@ -1,12 +1,25 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class ShowMan{
+class ShowMan extends StatefulWidget {
   static const MethodChannel _channel = const MethodChannel('agilefacil_mob');
+
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
+  @override
+  _ShowManState createState() => _ShowManState();
+}
+
+class _ShowManState extends State<ShowMan> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("ShowMan"),
+    );
+  }
 }
