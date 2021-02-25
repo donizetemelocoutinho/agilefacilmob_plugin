210594322a212b510705ad380bfeb7ef00d9e04e
donizetemelocoutinho <donizete.coutinho@agilefacil.com.br>
2021-02-25 14:12:40 -0300

    Teste de Plugion...

--- lib/ShowMan.dart
+++ lib/ShowMan.dart
@@ -1,3 +1,12 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
 class ShowMan{
+  static const MethodChannel _channel = const MethodChannel('agilefacil_mob');
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
 
 }
