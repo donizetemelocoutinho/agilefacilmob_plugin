c0835ab88d6bba93b844ede890ff6cd7feb2440e
donizetemelocoutinho <donizete.coutinho@agilefacil.com.br>
2021-02-25 14:07:36 -0300

    Teste de Plugion...

--- lib/agilefacil_mob.dart
+++ lib/agilefacil_mob.dart
@@ -4,11 +4,12 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class AgilefacilMob {
-  static const MethodChannel _channel =
-      const MethodChannel('agilefacil_mob');
+  static const MethodChannel _channel = const MethodChannel('agilefacil_mob');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+
 }
