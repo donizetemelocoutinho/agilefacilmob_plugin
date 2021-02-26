9b101ed15c1ef5f83961c73c0da9cffd18c1c414
donizetemelocoutinho <donizete.coutinho@agilefacil.com.br>
2021-02-26 11:30:36 -0300

    Melhorias na Helper

--- lib/helpers/helper.dart
+++ lib/helpers/helper.dart
@@ -1,9 +1,17 @@
 import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:uuid/uuid.dart';
 import 'package:device_info/device_info.dart';
 
 
 class Helper{
+  static const MethodChannel _channel = const MethodChannel('agilefacil_mob');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
 
   static List<String> UFs = ["","AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS",
   "MG","PA","PB","PR","PE","PI","RJ","RN","RS", "RO","RR","SC","SP","SE","TO"];
@@ -50,6 +58,16 @@ class Helper{
       }
     }
     return pos;
+  }
+
+
+  static DateTime strToDateTime(String date){
+    if (!(date.isEmpty || date == null))
+      return DateTime.parse(date);
+  }
+
+  static String DateTimeToStr(DateTime date){
+    return date.toString();
   }
 
 
