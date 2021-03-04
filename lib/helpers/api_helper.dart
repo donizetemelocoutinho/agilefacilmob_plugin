59381b9c9099618b7864292d648d4f87745145cc
donizetemelocoutinho <donizete.coutinho@agilefacil.com.br>
2021-03-04 16:32:58 -0300

    Atualização de pacotes

--- lib/helpers/api_helper.dart
+++ lib/helpers/api_helper.dart
@@ -6,11 +6,11 @@ class ApiHelper{
   static const _url_api = "api.agilefacil.com.br";
 
   Future<Map> get(String service,{Map<String, dynamic> params}) async {
-    String url = "";
+    Uri url;
     if (params != null)
-      url = Uri.https(_url_api,"/api/v1/" + service,params).toString();
+      url = Uri.https(_url_api,"/api/v1/" + service,params);
     else
-      url = Uri.https(_url_api,"/api/v1/" + service).toString();
+      url = Uri.https(_url_api,"/api/v1/" + service);
 
     var response = await http.get(url);
     var jr = convert.jsonDecode(response.body);
@@ -18,11 +18,11 @@ class ApiHelper{
   }
 
   Future<Map> post(String service,Map map,{Map<String, String> params}) async {
-    String url = "";
+    Uri url;
     if (params != null)
-      url = Uri.https(_url_api,"/api/v1/" + service,params).toString();
+      url = Uri.https(_url_api,"/api/v1/" + service,params);
     else
-      url = Uri.https(_url_api,"/api/v1/" + service).toString();
+      url = Uri.https(_url_api,"/api/v1/" + service);
 
     var response = await http.post(url,body: map);
     return convert.jsonDecode(response.body);
