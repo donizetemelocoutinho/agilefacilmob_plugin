32bff85a50f86d6ab4cf67edfdc13c46c05390ea
macbook <super3214>
2021-03-29 16:30:24 -0300

    get linha no endereco

--- lib/ui/acl_login.dart
+++ lib/ui/acl_login.dart
@@ -103,6 +103,7 @@ class _AclLoginState extends State<AclLogin> {
                               onPressed: () async {
                                 UsuarioApi u = UsuarioApi();
                                 Map ru = await u.Autenticar(Helper.loja["cpfcnpj"], _login.text, _senha.text);
+                                print(ru);
                                 if (ru["id"] == 0) {
                                   bool allow = await _allow(ru["api_token"]);
                                   if (allow)
