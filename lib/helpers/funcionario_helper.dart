bd1986ef9258edee4545b3c96c3064f320a093f5
Bruna Barbosa <bruna.silva@agilefacil.com.br>
2021-05-14 09:30:40 -0300

    Inclusão de parametros

--- lib/helpers/funcionario_helper.dart
+++ lib/helpers/funcionario_helper.dart
@@ -1,6 +1,26 @@
 
 import 'package:agilefacil_mob/helpers/pessoa_helper.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+import 'api_helper.dart';
+
+class FuncionarioApi {
+
+  static const MethodChannel _channel = const MethodChannel('agilefacil_mob');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  Future<Map> get(@required int codloja, @required int codfuncionario, @required String api_token) async{
+    ApiHelper api = ApiHelper();
+    return await api.get("funcionario/edit", params : {"codloja": codloja.toString(),"codfuncionario": codfuncionario.toString(),"api_token": api_token });
+  }
+
+}
+
 
 class Funcionario extends PessoaFisica{
   int codigo;
