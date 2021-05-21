dc9fbd4efbb20d62268babddb1c683bdef6e9bcc
Bruna Barbosa <bruna.silva@agilefacil.com.br>
2021-05-21 08:13:25 -0300

    GrupoApi

--- lib/helpers/estoque/grupo_helper.dart
+++ lib/helpers/estoque/grupo_helper.dart
@@ -1,3 +1,23 @@
+import 'package:agilefacil_mob/helpers/api_helper.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+class GrupoApi{
+  static const MethodChannel _channel = const MethodChannel('agilefacil_mob');
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+ Future<Map>getList({@required int codloja,@required String api_token, bool diff})async{
+    ApiHelper api = ApiHelper();
+    return await api.get("estoque/grupo/list", params:{"codloja":codloja.toString(), "api_token": api_token, "diff":diff});
+
+ }
+}
+
+
+
 class Grupo{
   int id;
   String guid;
