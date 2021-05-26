47ffbcdc0e31b982d359a750cbfb8bffdf239565
Bruna Barbosa <bruna.silva@agilefacil.com.br>
2021-05-26 09:30:21 -0300

    Inclusão FormadePagamentoHelper

---
+++ lib/helpers/formapagamento_helper.dart
@@ -0,0 +1,89 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+import 'api_helper.dart';
+
+class FormaPagamentoApi {
+  static const MethodChannel _channel = const MethodChannel('agilefacil_mob');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  Future<Map>getFormaPagamentoList({@required int codloja, @required int tipo, @required String api_token})async{
+    ApiHelper api = ApiHelper();
+    return await api.get("loja/pgto/list", params: {"codloja": codloja.toString(),"tipo": tipo.toString(),"api_token":api_token.toString()});
+  }
+}
+
+class FormaPagamento{
+
+  int id;
+  String guid;
+  int codformapagamento;
+  int codloja;
+  String idloja;
+  int indice;
+  int tipo;
+  int tipoconta;
+  bool padrao;
+  int finalidade;
+  int tpnfe;
+  String descricao;
+  String icone;
+
+  FormaPagamento();
+
+  FormaPagamento.fromMap(Map map){
+    id = map['id'];
+    guid = map['guid'];
+    codformapagamento = map['codformapagamento'];
+    codloja = map['codloja'];
+    idloja = map['idloja'];
+    indice = map['indice'];
+    tipo = map['tipo'];
+    tipoconta = map['tipoconta'];
+    padrao = map['padrao'];
+    finalidade = map['finalidade'];
+    tpnfe = map['tpnfe'];
+    descricao = map['descricao'];
+    icone = map['icone'];
+  }
+}
+
+
+class FormaPagamentoList{
+  String guid;
+  int codformapagamento;
+  int codloja;
+  String idloja;
+  int indice;
+  int tipo;
+  int tipoconta;
+  bool padrao;
+  int finalidade;
+  int tpnfe;
+  String descricao;
+  String icone;
+
+  FormaPagamentoList();
+
+  FormaPagamentoList.fromMap(Map map){
+
+    guid = map['guid'];
+    codformapagamento = map['codformapagamento'];
+    codloja = map['codloja'];
+    idloja = map['idloja'];
+    indice = map['indice'];
+    tipo = map['tipo'];
+    tipoconta = map['tipoconta'];
+    padrao = map['padrao'];
+    finalidade = map['finalidade'];
+    tpnfe = map['tpnfe'];
+    descricao = map['descricao'];
+    icone = map['icone'];
+  }
+}
