4295213b8838e203a5d3a8720e03a39c5d0fb183
DenisMuzeti <60753649+DenisMuzeti@users.noreply.github.com>
2021-10-01 10:20:50 -0300

    Criamos Class DispositivoApi

---
+++ lib/helpers/dispositivo.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'api_helper.dart';
+
+class DispositivoApi{
+  static const MethodChannel _channel = const MethodChannel('agilefacil_mob');
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  Future<Map>storeDispostivo({@required String api_token, @required Dispositivo dispositivo})async{
+    ApiHelper api = ApiHelper();
+    Map<String,dynamic> jo = dispositivo.toMap();
+    return await api.post("dispositivo/cadastrar", jo,params:{"api_token": api_token});
+
+  }
+
+}
+
+class Dispositivo{
+  String cpfcnpj = "";
+  String login = "";
+  String senha = "";
+  String celular = "";
+  String id = "";
+  String descricao = "";
+  String guid = "";
+
+  Dispositivo();
+
+  Dispositivo.fromMap(Map map){
+    cpfcnpj = map['cpfcnpj'];
+    login = map['login'];
+    senha = map['senha'];
+    celular = map['celular'];
+    id = map['id'];
+    descricao = map['descricao'];
+    guid = map['guid'];
+
+  }
+
+  @override
+  Map<String,dynamic> toMap() {
+    Map<String, dynamic> map = {
+      'cpfcnpj': cpfcnpj,
+      'login': login,
+      'senha': senha,
+      'celular': celular,
+      'id': id,
+      'descricao': descricao,
+      'guid': guid,
+    };
+    return map;
+  }
+}
