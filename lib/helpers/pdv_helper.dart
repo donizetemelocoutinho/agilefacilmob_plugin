724c8bb962df34a6b570eac5f2859aa865a65c7b
DenisMuzeti <60753649+DenisMuzeti@users.noreply.github.com>
2021-11-09 08:51:04 -0300

    Ajuste Class Dispositivo

---
+++ lib/helpers/pdv_helper.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+import 'api_helper.dart';
+import 'helper.dart';
+
+
+class Pdv{
+  static const MethodChannel _channel = const MethodChannel('agilefacil_mob');
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  Future<Map>Check(@required String codloja, @required String device) async{
+    ApiHelper api = ApiHelper();
+    Map<String,dynamic> jo = Map();
+    jo['id'] = await Helper.getId();
+    jo['device'] = device;
+    return await api.get("pdv/check", params : {"codloja": codloja,"device": device});
+  }
+
+  Future<Map> Store(@required String codloja, String api_token, String descricao) async{
+    ApiHelper api = ApiHelper();
+    Map<String,dynamic> jo = Map();
+    jo['id'] = await Helper.loja.codloja.toString();
+    jo['descricao'] = descricao;
+    return await api.post("owner/store", jo);
+  }
+
+
+}
