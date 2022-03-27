da2a21f607028b93091c39aa9cd1063ed2930473
DenisMuzeti <denismuzeti@hotmail.com>
2022-03-27 01:36:16 -0300

    Ajuste Usuario_helper

--- lib/helpers/loja_helper.dart
+++ lib/helpers/loja_helper.dart
@@ -63,7 +63,7 @@ class Loja{
     seguimento = map['seguimento'];
     tpinscricao = map['tpinscricao'];
     cpfcnpj = map['cpfcnpj'];
-    ie = map['ie'] ?? 0;
+    ie = map['ie'];
     im = map['im'];
     regimetrib = map['regimetrib'];
     endereco = Endereco.fromMap(map['endereco']);
