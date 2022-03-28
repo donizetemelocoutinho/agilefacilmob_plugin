e999e4375e4a2052c93727967a7d98592a9a809f
mob <denismuzeti@gmail.com>
2022-03-28 17:01:28 -0300

    Ajustes..

--- lib/helpers/loja_helper.dart
+++ lib/helpers/loja_helper.dart
@@ -62,8 +62,8 @@ class LojaResponsavel {
       seguimento = map['seguimento'];
       tpinscricao = map['tpinscricao'];
       cpfcnpj = map['cpfcnpj'];
-      ie = map['ie'] == null ? "0" : "${ie.toString()}";
-      im = map['im'] == null ? "0" : "${im.toString()}";
+      ie = map['ie'] ?? "".toString();
+      im = map['im'] ?? "".toString();
       regimetrib = map['regimetrib'];
       endereco = Endereco.fromMap(map['endereco']);
       fone = map['fone'];
