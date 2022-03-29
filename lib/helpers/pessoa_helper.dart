49c27681d6332388993fe7e8b24d8e478c137408
mob <denismuzeti@gmail.com>
2022-03-29 11:55:37 -0300

    Ajustes.. 29/03

--- lib/helpers/pessoa_helper.dart
+++ lib/helpers/pessoa_helper.dart
@@ -38,7 +38,7 @@ class Pessoa{
     codloja = map['codloja'];
     idloja = map['idloja'];
     tipo = map['tipo'];
-    contato = Contato.fromMap(map['contato'] ?? 0);
+    contato = Contato.fromMap(map['contato'] == null ? 0 : map['contato']);
     cadastro = Modification.fromMap(map['cadastro']);
 
     if (map['infobancolist'] != null){
