ab18acdf9aca2a538b964beadc25292c8ad50d58
mob <denismuzeti@gmail.com>
2022-03-30 18:10:48 -0300

    Ajustes.. 30/03

--- lib/helpers/pessoa_helper.dart
+++ lib/helpers/pessoa_helper.dart
@@ -50,7 +50,7 @@ class PessoHelper{
       ie = map['ie'];
       tipoie = map['tipoie'] ?? 0;
       obs = map['obs']?.toString() ?? "";
-      codgrdocumento = map['codgrdocumento']  == null ? "" : codgrdocumento = map['codgrdocumento'];
+      codgrdocumento = map['codgrdocumento'] ?? 0;
       endereco = Endereco.fromMap(map['endereco']);
 
     }
