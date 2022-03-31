1dc52fe6bca2c56cc4de3d9807acdc5d62325aee
mob <denismuzeti@gmail.com>
2022-03-31 09:12:00 -0300

    Denis 31/03 08:40

--- lib/helpers/pessoa_helper.dart
+++ lib/helpers/pessoa_helper.dart
@@ -107,7 +107,7 @@ class PessoHelper{
       super.fromMap(map);
 
       rg = map['rg'] == null ? "" : rg = map['rg'];
-      nascimento =  DateTime.tryParse(map['nascimento'])!;
+      nascimento =  DateTime.tryParse(map['nascimento']?.toString() ?? "")!;
       photo_link = map['foto'];
       sexo = map['sexo'];
       estadocivil = map['estadocivil'];
