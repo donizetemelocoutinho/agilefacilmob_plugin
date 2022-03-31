c88606a344b7d36cd31d220dd708fe98a39ca999
DenisMuzeti <denismuzeti@hotmail.com>
2022-03-30 22:31:42 -0300

    Denis 30/03 22:12

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -1,6 +1,6 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'db_counts.dart';
 
 class DBHelper{
