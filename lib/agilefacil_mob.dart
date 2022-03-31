82c49351068bc12267f6737f39174180e49c851a
mob <denismuzeti@gmail.com>
2022-03-31 10:44:49 -0300

    Denis 31/03 08:40

--- lib/agilefacil_mob.dart
+++ lib/agilefacil_mob.dart
@@ -7,6 +7,7 @@ class AgilefacilMob {
   static const MethodChannel _channel = const MethodChannel('agilefacil_mob');
 
   static Future<String> get platformVersion async {
+
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
