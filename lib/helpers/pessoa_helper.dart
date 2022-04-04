99bbaae2d28aef85e20d28f4385f73d84fc4866d
mob <denismuzeti@gmail.com>
2022-04-04 17:25:45 -0300

    Denis 01/04 15:07...

--- lib/helpers/pessoa_helper.dart
+++ lib/helpers/pessoa_helper.dart
@@ -106,7 +106,7 @@ class PessoHelper{}
 
       rg = map['rg'] == null ? "" : rg = map['rg'];
       nascimento =  DateTime.tryParse(map['nascimento'] ?? "");
-      photo_link = map['foto'] ?? "";
+      photo_link = map['foto']?.toString() ?? "";
       sexo = map['sexo'];
       estadocivil = map['estadocivil'];
       conjuge = map['conjuge'] != null ? Conjuge.fromMap(map['conjuge']) : null;
