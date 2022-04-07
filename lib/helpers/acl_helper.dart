e98756aa950bd51519540ce0a6a314b40276cbc0
mob <denismuzeti@gmail.com>
2022-04-07 17:14:20 -0300

    Denis 07/04 ...

--- lib/helpers/acl_helper.dart
+++ lib/helpers/acl_helper.dart
@@ -20,12 +20,12 @@ class AclHelper{
     return r;
   }
 
-  Future<bool?> _autentic() async{
+  Future<bool> _autentic() async{
     bool allow = await Navigator.push(context, MaterialPageRoute(builder: (context) => AclLogin(codloja: codloja, acl: acl, operacao: operacao,observacao: observacao)));
     return (allow != null) ? allow : false;
   }
 
-  Future<bool?> allow() async {
+  Future<bool> allow() async {
     Map r = await _allow();
     if (r["id"] == 0) {
       if (r["force_login"] == "N")
