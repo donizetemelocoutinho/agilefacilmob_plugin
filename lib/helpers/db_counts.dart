c560e9da70ec1161b5fe36f7267021d82dcb47e5
Denis <denismuzeti@gmail.com>
2022-05-10 09:15:16 -0300

    Ok

--- lib/helpers/db_counts.dart
+++ lib/helpers/db_counts.dart
@@ -152,6 +152,7 @@ class DBCount{
     empresalogolink = map[empresalogolinkColumn];
     loginfotolink = map[loginfotolinkColumn];
     usuario = convert.jsonDecode(map[jusuarioColumn]);
+
     loja = convert.jsonDecode(map[jlojaColumn]);
     celular = map[celularColumn];
     descricao = map[descricaoColumn];
