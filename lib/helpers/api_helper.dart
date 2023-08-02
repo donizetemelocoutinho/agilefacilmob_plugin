ca91ebb1529c5bf530de6d851e92031e5cf22de1
DenisMuzeti <denismuzeti@gmail.com>
2023-08-02 09:34:22 -0300

    Corrigido
    static const _url_api = "api.agilefacil.com.br";
    
        Uri url;
        if (params != null)
          url = Uri.https(_url_api,"/api/v1/" + service,params);
        else
          url = Uri.https(_url_api,"/api/v1/" + service);

--- lib/helpers/api_helper.dart
+++ lib/helpers/api_helper.dart
@@ -3,14 +3,14 @@ import 'package:http/http.dart' as http;
 
 class ApiHelper{
 
-  static const _url_api = "xapi.agilefacil.com.br";
+  static const _url_api = "api.agilefacil.com.br";
 
   Future<dynamic> get(String service,{required Map<String, dynamic> params}) async {
     Uri url;
     if (params != null)
-      url = Uri.https(_url_api,"/v1/" + service,params);
+      url = Uri.https(_url_api,"/api/v1/" + service,params);
     else
-      url = Uri.https(_url_api,"/v1/" + service);
+      url = Uri.https(_url_api,"/api/v1/" + service);
 
     var response = await http.get(url);
     var jr = jsonDecode(response.body);
@@ -20,9 +20,9 @@ class ApiHelper{
   Future<dynamic> post(String service,Map<String,dynamic> map,{required Map<String, String> params}) async {
     Uri url;
     if (params != null)
-      url = Uri.https(_url_api,"/v1/" + service,params);
+      url = Uri.https(_url_api,"/api/v1/" + service,params);
     else
-      url = Uri.https(_url_api,"/v1/" + service);
+      url = Uri.https(_url_api,"/api/v1/" + service);
 
     var response = await http.post(url,body: jsonEncode(map),headers: {"content-type": "application/json"});
     return jsonDecode(response.body);
